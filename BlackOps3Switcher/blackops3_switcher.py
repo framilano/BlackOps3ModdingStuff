9758c36e2c25ba67ca22d398a5438db6dea65874
Francesco Milano <milano.francesco@protonmail.com>
2026-01-07 23:29:32 +0100

    Added button to launch enhanced or mod tool

--- BlackOps3Switcher/blackops3_switcher.py
+++ BlackOps3Switcher/blackops3_switcher.py
@@ -1,43 +1,47 @@
 import json
-import os
-import msvcrt
-
+from subprocess import Popen
+from os import listdir, rename
+from msvcrt import getch
 
 def load_config():
     with open("config.json", "r") as f:
         config = json.load(f)
         return config
 
+config = load_config()
 
-def main():
-    config = load_config()
+def rename_to_correct_folder(mode):
     common_path = config["steam_common_path"]
 
-    list_of_folders = os.listdir(common_path)
+    list_of_folders = listdir(common_path)
     current_bo3_folder = common_path + "\\Call of Duty Black Ops III"
     enhanced_folder = common_path + "\\Call of Duty Black Ops III [Enhanced]"
     modtool_folder = common_path + "\\Call of Duty Black Ops III [ModTool]"
     
-    mode = ""
+    if ("Call of Duty Black Ops III [ModTool]" in list_of_folders and mode == "bo3modtool"):
+        rename(current_bo3_folder, enhanced_folder)
+        rename(modtool_folder, current_bo3_folder)
+        print("Renamed to BO3 Mod Tool.")
+    elif ("Call of Duty Black Ops III [Enhanced]" in list_of_folders and mode == "bo3enhanced"):
+        rename(current_bo3_folder, modtool_folder)
+        rename(enhanced_folder, current_bo3_folder)
+        print("Renamed to Enhanced BO3.")
+    elif ("Call of Duty Black Ops III [ModTool]" in list_of_folders and mode == "bo3enhanced"): print("Starting Enhanced BO3.")
+    elif ("Call of Duty Black Ops III [Enhanced]" in list_of_folders and mode == "bo3modtool"): print("Starting BO3 Mod Tool.")
+    else: print("BO3 not found, check your steam_common_path in config.json")
 
-    if ("Call of Duty Black Ops III [ModTool]" in list_of_folders):
-        os.rename(current_bo3_folder, enhanced_folder)
-        os.rename(modtool_folder, current_bo3_folder)
-        print("Switched to BO3 Mod Tool.")
-        mode = "bo3modtool"
-    elif ("Call of Duty Black Ops III [Enhanced]" in list_of_folders):
-        os.rename(current_bo3_folder, modtool_folder)
-        os.rename(enhanced_folder, current_bo3_folder)
-        print("Switched to Enhanced BO3.")
-        mode = "bo3enhanced"
-    else:
-        print("BO3 not found, check your steam_common_path in config.json")
+def main():
+    print("Wanna launch BO3 (E)nhanced or BO3 (M)od Tool?")
 
-    print("Launch it? y/N")
-    answer = msvcrt.getch().strip().lower()
-    if (answer == b'y' or answer == b''):
-        if (mode == "bo3enhanced"): os.system("\"C:\\Program Files (x86)\\Steam\\steam.exe\" steam://rungameid/311210")
-        else: os.system("\"C:\\Program Files (x86)\\Steam\\steam.exe\" steam://rungameid/455130")
+    answer = getch().strip().lower()
+
+    if (answer == b'e'):
+        rename_to_correct_folder("bo3enhanced")
+        Popen("C:\\Program Files (x86)\\Steam\\steam.exe steam://rungameid/311210".split(" "))
+    elif (answer == b'm'):
+        rename_to_correct_folder("bo3modtool")
+        Popen("C:\\Program Files (x86)\\Steam\\steam.exe steam://rungameid/455130".split(" "))
+
 
 
 if (__name__ == "__main__"): main()
